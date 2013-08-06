b3fe6c9cf77c2167a8aadd08a2c6abc0565dfec1
Peter Li <yang.li@iaspec.com>
2013-08-06 17:56:46 +0800

    add login,main,project add pages.

---
+++ web/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Peter
+ * Date: 13-8-6
+ * Time: 下午12:20
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+(function($){
+
+
+})(jQuery);
---
+++ web/js/project/project.js
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Peter
+ * Date: 13-8-6
+ * Time: 下午3:28
+ * To change this template use File | Settings | File Templates.
+ */
+(function($){
+
+//    alert($('#a_id_addBookmark').html());
+//    $('#a_id_addBookmark').on('onmouseover', function(){
+//        alert('aa');
+//    });
+
+})(jQuery);
