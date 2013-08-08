96332c0ada248a8222b17e54ceb19d98b6f45773
Peter Li <yang.li@iaspec.com>
2013-08-08 18:02:27 +0800

    add bug page and editable.js details supports.

--- web/js/main.js
+++ web/js/main.js
@@ -9,11 +9,56 @@
 
 (function ($) {
     var __website_prefix__ = '/heroapp/';
+
+    $.fn.exform = function(){
+        this.each(function () {
+            var form = $(this);
+            for (var i = $.fn.exform.renders.length - 1; i >= 0; i--) {
+                $.fn.exform.renders[i](form)
+            };
+            form.addClass('rended');
+        })
+    }
+    $.fn.exform.renders = [];
+    $(function() {
+        $('.exform:not(.rended)').exform();
+    });
+
+    $.getCookie = function(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+// Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+
     $(function () {
         $('#footer').addClass('clearfix').load(__website_prefix__+'footer.html');
         $('#top-nav').addClass('navbar navbar-inverse navbar-fixed-top').load(__website_prefix__+'topnav.html');
         $('#left-side').load(__website_prefix__+'left-side.html');
+
+
+        //toggle class button
+        $('body').on('click.xa.togglebtn.data-api', '[data-toggle=class]', function (e) {
+            var $this = $(this), href
+            var target = $this.attr('data-target')
+                || e.preventDefault()
+                || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, ''); //strip for ie7
+            var className = $this.attr('data-class-name');
+            $(target).toggleClass(className);
+        });
     });
+
+
 
 })(jQuery);
 
--- web/js/page.list.js
+++ web/js/page.list.js
@@ -1,21 +1,15 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Peter
- * Date: 13-8-7
- * Time: 下午1:58
- * To change this template use File | Settings | File Templates.
- */
+
 jQuery(function ($) {
 //full screen btn
     $('.layout-btns .layout-full').click(function (e) {
         if ($(this).hasClass('active')) {
 // reset
-            $('#left-side, ul.breadcrumb').show('fast');
-            $('#content-block').removeClass('col-lg-12 full-content').addClass('col-lg-10');
+            $('#left-side, ul.breadcrumb').show('fast').removeProp('style');
+            $('#content-block').removeClass('col-sm-12 col-lg-12 full-content').addClass('col-sm-11 col-lg-10');
         } else {
 // full screen
             $('#left-side, ul.breadcrumb').hide('fast', function () {
-                $('#content-block').removeClass('col-lg-10').addClass('col-lg-12 full-content');
+                $('#content-block').removeClass('col-sm-11 col-lg-10').addClass('col-sm-12 col-lg-12 full-content');
             });
         }
     });
